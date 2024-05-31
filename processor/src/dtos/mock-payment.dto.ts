0b04f40a5463eda92b5ca4a62571b8dd453589fa
Aaron Meza <aronmeza1@gmail.com>
2024-05-31 15:58:55 -0600

    #57 Refactor payment creation and import methods in Stripe service
    The implementation of the payment creation function in StripePaymentService has been refactored to provide initial data to cart for payment creation in external PSPs. Simultaneously, the payment method schema in mock-payment.dto.ts has been simplified and adjusted.

--- processor/src/dtos/mock-payment.dto.ts
+++ processor/src/dtos/mock-payment.dto.ts
@@ -1,14 +1,8 @@
 import { Static, Type } from '@sinclair/typebox';
 
 export const CardPaymentMethodSchema = Type.Object({
-  // TODO: Remove the fields according to the payment provider solution,
-  //  Strongly recommend not to process PAN data to Connectors.
-  type: Type.Literal('card'),
-  cardNumber: Type.String(),
-  expiryMonth: Type.Number(),
-  expiryYear: Type.Number(),
-  cvc: Type.Number(),
-  holderName: Type.Optional(Type.String()),
+  type: Type.String(),
+  paymentIntent: Type.String(),
 });
 
 export const PaymentRequestSchema = Type.Object({
@@ -18,6 +12,7 @@ export const PaymentRequestSchema = Type.Object({
 export enum PaymentOutcome {
   AUTHORIZED = 'Authorized',
   REJECTED = 'Rejected',
+  INITIAL = 'Initial',
 }
 
 export const PaymentOutcomeSchema = Type.Enum(PaymentOutcome);
