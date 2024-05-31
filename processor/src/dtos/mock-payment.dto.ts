23611e33cbbd7334de4bd9a50f84f4615e5dbcf9
Aaron Meza <aronmeza1@gmail.com>
2024-05-31 16:32:58 -0600

    #34 Simplify mock-payment.dto and remove stripeClientSecret from config
    The mock-payment.dto has been greatly simplified by removing numerous unused or unnecessary data schemas. The stripeClientSecret has been removed from the config since it is no longer needed. This commit helps in reducing complexity and improving readability.

--- processor/src/dtos/mock-payment.dto.ts
+++ processor/src/dtos/mock-payment.dto.ts
@@ -15,64 +15,8 @@ export const PaymentRequestSchema = Type.Object({
   paymentMethod: Type.Composite([CardPaymentMethodSchema]),
 });
 
-export const AmountDetailsSchema = Type.Object({
-  tip: Type.Object({}),
-});
-export const AutomaticPaymentMethodsSchema = Type.Object({
-  enabled: Type.Boolean(),
-});
-export const PaymentMethodOptionsSchema = Type.Object({
-  card: Type.Object({
-    installments: Type.Null(),
-    mandate_options: Type.Null(),
-    network: Type.Null(),
-    request_three_d_secure: Type.String(),
-  }),
-  link: Type.Object({
-    persistent_token: Type.Null(),
-  }),
-});
-
 export const PaymentIntentResponseSchema = Type.Object({
-  id: Type.String(),
-  object: Type.String(),
-  amount: Type.Number(),
-  amount_capturable: Type.Number(),
-  amount_details: AmountDetailsSchema,
-  amount_received: Type.Number(),
-  application: Type.Null(),
-  application_fee_amount: Type.Null(),
-  automatic_payment_methods: AutomaticPaymentMethodsSchema,
-  canceled_at: Type.Null(),
-  cancellation_reason: Type.Null(),
-  capture_method: Type.String(),
   client_secret: Type.String(),
-  confirmation_method: Type.String(),
-  created: Type.Number(),
-  currency: Type.String(),
-  customer: Type.Null(),
-  description: Type.Null(),
-  invoice: Type.Null(),
-  last_payment_error: Type.Null(),
-  latest_charge: Type.Null(),
-  livemode: Type.Boolean(),
-  metadata: Type.Object({}),
-  next_action: Type.Null(),
-  on_behalf_of: Type.Null(),
-  payment_method: Type.Null(),
-  payment_method_options: PaymentMethodOptionsSchema,
-  payment_method_types: Type.Array(Type.String()),
-  processing: Type.Null(),
-  receipt_email: Type.Null(),
-  review: Type.Null(),
-  setup_future_usage: Type.Null(),
-  shipping: Type.Null(),
-  source: Type.Null(),
-  statement_descriptor: Type.Null(),
-  statement_descriptor_suffix: Type.Null(),
-  status: Type.String(),
-  transfer_data: Type.Null(),
-  transfer_group: Type.Null(),
 });
 
 export enum PaymentOutcome {
