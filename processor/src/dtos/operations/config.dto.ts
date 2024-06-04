c886bb821a115bab80a605ef35d56d62a82ef8f7
Aaron Meza <aronmeza1@gmail.com>
2024-06-03 18:17:59 -0600

    #68 Update and test operations.spec.ts processor API
    Update and test the config and status method operations.spec.ts.

--- processor/src/dtos/operations/config.dto.ts
+++ processor/src/dtos/operations/config.dto.ts
@@ -1,5 +1,8 @@
 import { Static, Type } from '@sinclair/typebox';
 
-export const ConfigResponseSchema = Type.Any();
+export const ConfigResponseSchema = Type.Object({
+  clientKey: Type.String(),
+  environment: Type.String(),
+});
 
 export type ConfigResponseSchemaDTO = Static<typeof ConfigResponseSchema>;
