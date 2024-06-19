04b754ac517b7cf07ded1e559c18b763bf113ba5
LucinaBueno <lucina.bueno@jwmsolutions.com>
2024-06-19 16:25:14 -0600

    #84 Improvement: Unit Test / Improve code coverage
    - Organize unit tests following a single structure.
    - Improve coverage on post-deploy script.
    - Delete service mock unit test.

--- processor/src/connectors/post-deploy.ts
+++ processor/src/connectors/post-deploy.ts
@@ -6,16 +6,21 @@ import { retrieveWebhookEndpoint, updateWebhookEndpoint } from './actions';
 const STRIPE_WEBHOOKS_ROUTE = 'stripe/webhooks';
 const CONNECT_SERVICE_URL = 'CONNECT_SERVICE_URL';
 const STRIPE_WEBHOOK_ID = 'STRIPE_WEBHOOK_ID';
+const msgError = 'Post-deploy failed:';
 
 async function postDeploy(properties: any) {
   const applicationUrl = properties.get(CONNECT_SERVICE_URL);
-  const stripeWebhookId = properties.get(STRIPE_WEBHOOK_ID) || '';
+  const stripeWebhookId = properties.get(STRIPE_WEBHOOK_ID) ?? '';
 
   if (properties) {
-    const we = await retrieveWebhookEndpoint(stripeWebhookId);
-    const weAppUrl = `${applicationUrl}${STRIPE_WEBHOOKS_ROUTE}`;
-    if (we.url !== weAppUrl) {
-      updateWebhookEndpoint(stripeWebhookId, weAppUrl);
+    if (stripeWebhookId === '') {
+      process.stderr.write(`${msgError} STRIPE_WEBHOOK_ID var is not assigned.\n`);
+    } else {
+      const we = await retrieveWebhookEndpoint(stripeWebhookId);
+      const weAppUrl = `${applicationUrl}${STRIPE_WEBHOOKS_ROUTE}`;
+      if (we.url !== weAppUrl) {
+        updateWebhookEndpoint(stripeWebhookId, weAppUrl);
+      }
     }
   }
 }
@@ -27,7 +32,7 @@ export async function runPostDeployScripts() {
     await postDeploy(properties);
   } catch (error) {
     if (error instanceof Error) {
-      process.stderr.write(`Post-deploy failed: ${error.message}\n`);
+      process.stderr.write(`${msgError} ${error.message}\n`);
     }
     process.exitCode = 1;
   }
